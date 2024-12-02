2d46f25db2e146a7ab8e54b76443b5c71578d128
RayanisRayan <Rayanalipersonal@gmail.com>
2024-12-02 14:29:55 +0300

    Update Config.py

--- src/RTISystem/Config.py
+++ src/RTISystem/Config.py
@@ -1,9 +1,7 @@
-SUPABASE_URL = "https://zymxgpzqnwphdnuyhzmn.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bXhncHpxbndwaGRudXloem1uIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjU2MjEzMiwiZXhwIjoyMDQ4MTM4MTMyfQ.JzL5XYS0mTdk0Bf_J6KTn8AkW2-L2DqFS7hL5q4BStk"
+SUPABASE_URL = "http://10.13.103.14:8000"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3MzMwMDA0MDAsCiAgImV4cCI6IDE4OTA3NjY4MDAKfQ.TIvUAyvZFK-PydwZD3ahHDsUFmI0tXOM6TZOPaoDcxc"
 HOST = "0.0.0.0"
 PORT = 8765
-DEPARTMENTS = ['Cardioligy']
-DOCTORS = [{"name":"Mohmmaed","department":"Cardioligy"}]
 QUEUE_HOST = '0.0.0.0'
 QUEUE_PORT = 8770
 DOCTOR_HOST = '0.0.0.0'
