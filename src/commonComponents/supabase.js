79053bbec1cc92c8854816973c7323b89d159ada
Rashed <rashedalmanie@gmail.com>
2024-12-10 20:38:43 +0300

    fix
    Co-Authored-By: Yazeed <94597714+Yazeed-Alharbi@users.noreply.github.com>

--- src/commonComponents/supabase.js
+++ src/commonComponents/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'; 
-const supabaseUrl = 'http://192.168.164.24:8000';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3MzMwMDA0MDAsCiAgImV4cCI6IDE4OTA3NjY4MDAKfQ.TIvUAyvZFK-PydwZD3ahHDsUFmI0tXOM6TZOPaoDcxc'; 
+const supabaseUrl = 'https://zymxgpzqnwphdnuyhzmn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bXhncHpxbndwaGRudXloem1uIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjU2MjEzMiwiZXhwIjoyMDQ4MTM4MTMyfQ.JzL5XYS0mTdk0Bf_J6KTn8AkW2-L2DqFS7hL5q4BStk'; 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase
+
